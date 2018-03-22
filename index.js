3d4a5e093d37d2a55dcaac20e627249f276db4c4
orthagonal <orthagonal1@gmail.com>
2018-03-21 20:30:22 -0500

    global timezone

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ const processEndpoint = async(endpointName, endpointSpec) => {
 // store all intervals so we can gracefull stop them later:
 const allIntervals = [];
 
-const registerEndpoint = (endpointName, endpointSpec) => {
+const registerEndpoint = (endpointName, endpointSpec, timezone) => {
   const executeInterval = () => {
     log([endpointName, 'notice', 'running'], `running ${endpointName}`);
     // 'endpoint' means it is a url to invoke:
@@ -63,7 +63,7 @@ const registerEndpoint = (endpointName, endpointSpec) => {
     onTick: executeInterval,
     start: true,
     runOnInit: endpointSpec.runNow,
-    timeZone: endpointSpec.timezone
+    timezone
   };
   const job = new CronJob(jobSpec);
   allIntervals.push(job);
@@ -85,7 +85,6 @@ module.exports = async(jobsPath) => {
     options.configFile = jobsPath;
   }
   const specs = await confi(options);
-
   // load a laterjs instance based on the timezone
   if (!specs.jobs) {
     throw new Error('no jobs found');
@@ -93,7 +92,7 @@ module.exports = async(jobsPath) => {
   const jobNames = Object.keys(specs.jobs);
   for (let i = 0; i < jobNames.length; i++) {
     const jobName = jobNames[i];
-    const registration = registerEndpoint(jobName, specs.jobs[jobName]);
+    const registration = registerEndpoint(jobName, specs.jobs[jobName], specs.timezone);
     if (registration instanceof Error) {
       throw registration;
     }
